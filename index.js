ee801691c94d3093fa91685de080e150ed0316c9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 01:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ BoardMember.prototype.doCharity = function(){
   return "I like to help people."
 }
 //
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
+BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+}
 //
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
