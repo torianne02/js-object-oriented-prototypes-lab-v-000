28a0b0d097a4d20c48939e432fac92306bf17f4c
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 01:27:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState;
   this.training = training;
 };
+
 //
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
+  User.prototype.veto = function(){
+    return 'No, I must disagree'
+  }
 //   this.approve = function(){
 //     return 'You can do that!'
 //   }
