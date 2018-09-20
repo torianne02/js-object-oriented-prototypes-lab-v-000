49674a2ccda05166c63db22309100bb9ae353430
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 01:28:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ BoardMember.prototype.approve = function(){
 
 BoardMember.prototype.doCharity = function(){
   return "I like to help people."
-}
-//
+};
+
 BoardMember.prototype.releasePressStatement = function(){
   return "You will see great things from Scuber."
-}
+};
+
 //
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
